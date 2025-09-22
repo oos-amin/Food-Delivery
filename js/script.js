dd50b8f3780304081424b49abf51ffbb76e224b6
Amin Moeini <amin47moeini@gmail.com>
2025-09-22 21:26:55 +0330

    complete header: html, css, scripts, responsive

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 'use strict'
+
+const navBtn = document.querySelector('.nav__toggle');
+const navMenu = document.querySelector('.nav-list');
+const overlay = document.querySelector('.overlay');
+const body = document.body;
+
+navBtn.addEventListener('click', function () {
+    navBtn.classList.toggle('nav__toggle-line--open');
+    navMenu.classList.toggle('nav-list--open');
+    overlay.classList.toggle('overlay--show');
+    if (navBtn.classList.contains('nav__toggle-line--open')) {
+        body.classList.add('no-scroll');
+    } else {
+        body.classList.remove('no-scroll');
+
+    }
+});
+
+overlay.addEventListener('click', function () {
+    overlay.classList.remove('overlay--show');
+    navMenu.classList.remove('nav-list--open');
+    navBtn.classList.remove('nav__toggle-line--open');
+    body.classList.remove('no-scroll');
+});
